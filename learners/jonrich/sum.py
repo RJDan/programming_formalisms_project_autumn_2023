88c87aee9d2b2a65e5f93d168d78a2e6f543bd31
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-28 14:27:22 +0100

    Check for input types

--- learners/jonrich/sum.py
+++ learners/jonrich/sum.py
@@ -2,3 +2,11 @@ def add_numbers(a, b):
     return a + b
 
 assert add_numbers(1, 2) == 3
+
+# SHould hive an error
+has_raised_error = False
+try:
+    add_numbers("nonsense", "irrelevant")
+except AssertionError:
+    has_raised_error = True
+assert has_raised_error
