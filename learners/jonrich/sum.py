91344e784385a8c4f58b23a0c5ef7cf3ad92d2af
jonandernovella <novella.uu@gmail.com>
2023-11-28 14:29:50 +0100

    type check and docs

--- learners/jonrich/sum.py
+++ learners/jonrich/sum.py
@@ -1,12 +1,15 @@
 def add_numbers(a, b):
+    assert isinstance(a, (int, float)), "a must be an integer"
+    assert isinstance(b, (int, float)), "b must be an integer"
     return a + b
 
 assert add_numbers(1, 2) == 3
 
-# SHould hive an error
 has_raised_error = False
 try:
     add_numbers("nonsense", "irrelevant")
 except AssertionError:
     has_raised_error = True
 assert has_raised_error
+
+assert add_numbers.__doc__
